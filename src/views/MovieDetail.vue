eb31960ef227de209cd64122edb6e73d38a011f6
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-12 20:04:59 +0100

    update movie detail page

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,29 +1,55 @@
 <template>
-  <div class="movie-detail">
-    <h2>{{ movie.Title }}</h2>
-  </div>
+    <div class="movie-detail">
+        <h2 class="title">{{ movie.Title }}</h2>
+        <p>Released: {{ movie.Year }}</p>
+        <img :src="movie.Poster" :alt="movie.Title">
+        <h3>Description:</h3>
+        <p>{{ movie.Plot }}</p>
+        <h3>Actors:</h3>
+        <span class="actor" v-for="(actor, index) in actors" :key="index">
+            {{ actor }}
+        </span>
+    </div>
 </template>
 
 <script>
 import env from "@/env";
 import { ref, onBeforeMount } from "vue";
-import { useRouter } from "vue-router";
+import { useRoute } from "vue-router";
 
 export default {
-  setup() {
-    const movie = ref({});
-    const route = useRoute();
-    const api_url = "https://www.omdbapi.com/"
+    setup() {
+        const movie = ref({});
+        const route = useRoute();
+        const actors = ref([]);
+        const api_url = "https://www.omdbapi.com/"
+        const id = route.params.id;
 
-    onBeforeMount(() => {
-        console.log(route.params.id);
-    })
+        onBeforeMount(() => {
+            fetch(`${api_url}?apikey=${env.apikey}&i=${id}&plot=full`)
+                .then(res => res.json())
+                .then(data => {
+                    movie.value = data;
+                    actors.value = data.Actors.split(",");
+                    console.log(data);
+                })
+        })
 
-    return {
-      movie,
-    };
-  },
+        return {
+            movie,
+        };
+    },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+    .movie-detail {
+        color: #fff;
+        padding: 20px;
+
+        .title {
+            font-size: 28px;
+            margin-bottom: 20px;
+        }
+    }
+</style>
